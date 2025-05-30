86e17e34f9f7b37435019c11956a5d2e473eaa7f
iAdvokat <sayaee@gmail.com>
2025-05-30 10:47:46 +0300

    Виконання двох задач

---
+++ hillel_python_basic.py
@@ -0,0 +1,22 @@
+# # home work 2.1
+# number = int(input("Введіть Ваша чотирьох значне число: "))
+#
+# n1 = number // 1000
+# n2 = number // 100 % 10
+# n3 = number // 10 % 10
+# n4 = number % 10
+#
+# print(f"{n1}\n{n2}\n{n3}\n{n4}")
+# # home work 2.2
+# number = int(input("Введіть п’ятизначне число: "))
+#
+# n1 = number // 10000
+# n2 = number // 1000 % 10
+# n3 = number // 100 % 10
+# n4 = number // 10 % 10
+# n5 = number % 10
+#
+# result = n5 * 10000 + n4 * 1000 + n3 * 100 + n2 * 10 + n1
+#
+# print(result)
+
