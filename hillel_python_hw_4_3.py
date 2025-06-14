f70500dca7735b1868f672cfc04318c43472a900
Oleksandr Neysalo <sayaee@gmail.com>
2025-06-14 12:11:42 +0300

    переделал дз согласно условий задачи.

--- hillel_python_hw_4_3.py
+++ hillel_python_hw_4_3.py
@@ -1,7 +1,19 @@
-nums = [1, 2, 3, 4, 5, 6, 7, 9]
+# nums = [1, 2, 3, 4, 5, 6, 7, 9]
+#
+# if len(nums) < 3:
+#     print("Здається у Вас бракує чисел.. Потрібно щонайменше 3. ;) ")
+# else:
+#     result = [nums[0], nums[2], nums[-2]]
+#     print(result)
 
-if len(nums) < 3:
-    print("Здається у Вас бракує чисел.. Потрібно щонайменше 3. ;) ")
-else:
-    result = [nums[0], nums[2], nums[-2]]
-    print(result)
+#v2
+
+import random
+
+length = random.randint(3, 10)
+nums = [random.randint(1, 20) for _ in range(length)]
+
+result = [nums[0], nums[2], nums[-2]]
+
+print("Початковий список:", nums)
+print("Результат:", result)
